a361830a0b8fb98387bd4586b67e07c940fdbb20
MrRobboWilliamson <49740636+MrRobboWilliamson@users.noreply.github.com>
2019-05-24 17:47:28 +1000

    deleted redundant files

--- static/main.js
+++
@@ -1,2 +0,0 @@
-// add event handler on click of "edit" to add a form that points to your edit view that has an 
-// textarea/input with the message
--- test_mysql.py
+++
@@ -1,6 +0,0 @@
-# testing mysql
-import mysql.connector as conn
-
-conn.connect()
-print('Hello World')
-
